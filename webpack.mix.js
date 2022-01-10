500b0a9a971fc57dd881c93724d38adb5b89b5fc
Yayan Sopian <38284814+SopianGithub@users.noreply.github.com>
2022-01-10 21:02:31 +0700

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,8 @@
-const mix = require('laravel-mix');
+const path = require('path')
+const mix = require('laravel-mix')
+const cssImport = require('postcss-import')
+const cssNesting = require('postcss-nesting')
+
 
 /*
  |--------------------------------------------------------------------------
@@ -6,27 +10,20 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
- mix.js('resources/js/app.js', 'public/js')
- .vue()
- .postCss('resources/css/app.css', 'public/css', [
-     require('postcss-import'),
-     require('tailwindcss'),
-     require('autoprefixer'),
- ])
- .webpackConfig(require('./webpack.config'));
-
-mix.options({
- hmrOptions: {
-     host: '127.0.0.1',  // mysite.test is my local domain used for testing
-     port: 8888,
- }
-});
-
-if (mix.inProduction()) {
- mix.version();
-}
+mix
+  .js('resources/js/app.js', 'public/js')
+  .vue({ runtimeOnly: true })
+  .alias({ '@': 'resources/js' })
+  .postCss('resources/css/app.css', 'public/css', [
+    // prettier-ignore
+    cssImport(),
+    cssNesting(),
+    require('tailwindcss'),
+  ])
+  .version()
+  .sourceMaps()
